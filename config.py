d1d508749cf92bb8cd0b5ea64d8bd8edb6bfa1c2
vladimT <91445179+vladimT@users.noreply.github.com>
2023-06-16 22:28:45 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5941290436:AAGwB52GjdNM-GCU2HrgLDsiIPyW7mbE3X0'
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'иена': 'JPY',
+}
---
+++ extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class CurrencyConverter:
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str):
+        if base == quote:
+            raise APIException(f'Невозможно перевести одинаковые валюты {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_quote = json.loads(r.content)[keys[quote]]
+        return total_quote * amount
